270ef1707a7b915dbcd7ebde8e56e02f0ad98c7e
kaweesha-mr <kaweesha.mr@gmail.com>
2025-02-02 18:07:47 +0530

    chore: group member DTO added

--- src/main/java/org/spring/authenticationservice/DTO/Members/GrpMember.java
+++ src/main/java/org/spring/authenticationservice/DTO/Members/GrpMember.java
@@ -1,4 +1,38 @@
 package org.spring.authenticationservice.DTO.Members;
 
 public class GrpMember {
+
+    private String username;
+    private Long memberId;
+    private String role;
+
+    public GrpMember(String username, Long memberId, String role) {
+        this.username = username;
+        this.memberId = memberId;
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
 }
