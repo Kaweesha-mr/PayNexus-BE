6f77f3fb218cbd49766348289437075bd6983bc4
kaweesha-mr <kaweesha.mr@gmail.com>
2025-02-02 18:08:11 +0530

    chore: repo updated to findGroup by id and status

--- src/main/java/org/spring/authenticationservice/repository/UserGroupRoleRepository.java
+++ src/main/java/org/spring/authenticationservice/repository/UserGroupRoleRepository.java
@@ -5,9 +5,13 @@ import org.spring.authenticationservice.model.User;
 import org.spring.authenticationservice.model.UserGroupRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserGroupRoleRepository extends JpaRepository<UserGroupRole,Long> {
     boolean existsByUserAndGroup( User user, Group group);
     Optional<UserGroupRole> findByUserIdAndGroupIdAndStatus(Long userId, Long groupId, UserGroupRole.InvitationStatus status);
+    List<UserGroupRole> findByGroupAndStatus(Group group, UserGroupRole.InvitationStatus status);
+
 }
+
